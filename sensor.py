bee5e4bf91e1ee5b4e03034e7bac3c4309785069
Diego Santiso <djs4000@yahoo.com>
2025-06-07 13:56:50 +0400

    updated sensor

--- sensor.py
+++ sensor.py
@@ -1,18 +1,39 @@
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import SensorEntity
+from .const import DOMAIN
 
-class TaskListSensor(Entity):
-    def __init__(self, name, tasks):
-        self._name = name
-        self._tasks = tasks
+class TodoSummarySensor(SensorEntity):
+    def __init__(self, hass, todo_lists):
+        self.hass = hass
+        self._todo_lists = todo_lists
+        self._attr_name = "Todo Summary"
+        self._attr_native_value = 0
+        self._attr_extra_state_attributes = {}
 
-    @property
-    def name(self):
-        return self._name
+    async def async_update(self):
+        total_tasks = 0
+        all_tasks = {}
 
-    @property
-    def state(self):
-        return len(self._tasks)
+        for entity_id in self._todo_lists:
+            try:
+                response = await self.hass.services.async_call(
+                    "todo",
+                    "get_items",
+                    {"entity_id": entity_id},
+                    blocking=True,
+                    return_response=True
+                )
 
-    @property
-    def extra_state_attributes(self):
-        return {"tasks": self._tasks}
+                tasks = response.get("items", [])
+                pending = [t["summary"] for t in tasks if t["status"] == "needs_action"]
+                total_tasks += len(pending)
+                all_tasks[entity_id] = pending
+
+            except Exception as e:
+                all_tasks[entity_id] = f"Error: {str(e)}"
+
+        self._attr_native_value = total_tasks
+        self._attr_extra_state_attributes = all_tasks
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    todo_lists = entry.data.get("todo_lists", [])
+    async_add_entities([TodoSummarySensor(hass, todo_lists)], update_before_add=True)
