6aa080bb381c0a0f9a7aa12b04ff7a0fef988259
Diego Santiso <djs4000@yahoo.com>
2025-06-14 12:32:01 +0400

    .

--- todo.js
+++ todo.js
@@ -1,20 +1,15 @@
 class MyTodoCard extends HTMLElement {
-  set hass(hass) {
-    if (!this.content) {
-      this.innerHTML = `<ha-card header="My Todo Lists">
-        <div class="card-content">
-          Loading...
-        </div>
-      </ha-card>`;
-      this.content = this.querySelector('.card-content');
-    }
+async set hass(hass) {
+  if (!this.content) {
+    this.innerHTML = `<ha-card header="${this.config.title || 'My Todo Lists'}">
+      <div class="card-content">Loading...</div>
+    </ha-card>`;
+    this.content = this.querySelector('.card-content');
+  }
 
-    const todoEntities = [
-      "todo.shopping_list",
-      "todo.home_stuff"
-    ];
-	
-	try {
+  const entities = this.config.entities;
+
+  try {
     const promises = entities.map(async (entity) => {
       const response = await hass.callWS({
         type: "call_service",
@@ -25,37 +20,39 @@ class MyTodoCard extends HTMLElement {
       });
 
       console.log(`Full response for entity ${entity}:`, response);
-/* 
-    Promise.all(todoEntities.map(entity => 
-      hass.callWS({
-        type: "call_service",
-        domain: "todo",
-        service: "get_items",
-        target: { entity_id: entity },
-		return_response: true
-      }).then(response => ({ entity, response }))
-    )).then(results => {
-      let html = '';
-      results.forEach(({entity, response}) => {
+
+      return { entity, response };
+    });
+
+    const results = await Promise.all(promises);
+
+    let html = '';
+    results.forEach(({ entity, response }) => {
+      // Adjust this once you confirm the exact structure from the console
+      if(response.response && response.response.items) {
         html += `<b>${entity.replace('todo.', '')}</b><ul>`;
-        response.items.forEach(item => {
+        response.response.items.forEach(item => {
           html += `<li>${item.summary}</li>`;
         });
-        html += `</ul>`;
-      });
- */
-      this.content.innerHTML = html;
-    }).catch(err => {
-      console.error(err);
-      this.content.innerHTML = "Error loading todo lists.";
+        html += '</ul>';
+      } else {
+        html += `<b>${entity.replace('todo.', '')}</b>: No items found.<br>`;
+      }
     });
+
+    this.content.innerHTML = html;
+
+  } catch (error) {
+    console.error("Error fetching todo items:", error);
+    this.content.innerHTML = "Error loading todo lists.";
   }
+}
 
   setConfig(config) {}
 
   getCardSize() {
     return 1;
   }
-//}
+}
 
 customElements.define('my-todo-card', MyTodoCard);
