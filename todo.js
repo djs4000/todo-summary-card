650384d024ed402c5a593d7a56ae398a30e29ba9
Diego Santiso <djs4000@yahoo.com>
2025-06-14 12:13:28 +0400

    .

--- todo.js
+++ todo.js
@@ -1,52 +1,45 @@
 class MyTodoCard extends HTMLElement {
-
-  setConfig(config) {
-    this.config = config;
-    if (!config.entities) {
-      throw new Error("You need to define todo entities");
-    }
-  }
-
-  async set hass(hass) {
+  set hass(hass) {
     if (!this.content) {
-      this.innerHTML = `<ha-card header="${this.config.title || 'My Todo Lists'}">
-        <div class="card-content">Loading...</div>
+      this.innerHTML = `<ha-card header="My Todo Lists">
+        <div class="card-content">
+          Loading...
+        </div>
       </ha-card>`;
       this.content = this.querySelector('.card-content');
     }
 
-    const entities = this.config.entities;
+    const todoEntities = [
+      "todo.shopping_list",
+      "todo.home_stuff"
+    ];
 
-    try {
-      const promises = entities.map(async (entity) => {
-        const response = await hass.callWS({
-          type: "call_service",
-          domain: "todo",
-          service: "get_items",
-          target: { entity_id: entity }
-		  return_response: true  // <-- explicitly required
-        });
-        return { entity, response };
-      });
-
-      const results = await Promise.all(promises);
-
+    Promise.all(todoEntities.map(entity => 
+      hass.callWS({
+        type: "call_service",
+        domain: "todo",
+        service: "get_items",
+        target: { entity_id: entity }
+		return_response: true  // <-- explicitly required
+      }).then(response => ({ entity, response }))
+    )).then(results => {
       let html = '';
-      results.forEach(({ entity, response }) => {
+      results.forEach(({entity, response}) => {
         html += `<b>${entity.replace('todo.', '')}</b><ul>`;
         response.items.forEach(item => {
           html += `<li>${item.summary}</li>`;
         });
-        html += '</ul>';
+        html += `</ul>`;
       });
 
       this.content.innerHTML = html;
-
-    } catch (error) {
-      console.error("Error fetching todo items:", error);
+    }).catch(err => {
+      console.error(err);
       this.content.innerHTML = "Error loading todo lists.";
-    }
+    });
   }
+
+  setConfig(config) {}
 
   getCardSize() {
     return 1;
